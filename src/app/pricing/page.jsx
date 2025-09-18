660a6e9030c3ccc1a05bfcd327d68c42d722e8eb
Hassam Shahryar <hassamytchannel77@gmail.com>
2025-09-18 23:54:02 +0100

    error solved vercel

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,14 +1,10 @@
 import PricingPage from '@/components/PricingPage'
-import WhatsAppButton from '@/components/WhatsAppButton'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='overflow-hidden'>
       <PricingPage/>
-      <section>
-        <WhatsAppButton/>
-      </section>
     </div>
   )
 }
